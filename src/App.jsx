abdb65aa2c386a2cf35d9515d27d91c345d4e4f8
Desai Priyansh <priyanshdesai62@gmail.com>
2025-08-12 17:59:14 +0530

    transition animation added to al pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import React from "react";
+import { AnimatePresence } from "framer-motion";
+
 import Login from "./pages/Login";
-import Layout from "./pages//Layout";
+import Layout from "./pages/Layout";
 import Dashboard from "./pages/Dashboard";
 import POPage from "./pages/POPage";
 import DispatchPage from "./pages/DispatchPage";
@@ -10,31 +12,119 @@ import GoodsIssueUpdates from "./pages/GoodsIssueUpdates";
 import ProjectsPage from "./pages/ProjectsPage";
 import Reports from "./pages/Reports";
 import UsersPage from "./pages/UsersPage";
+import AnimatedPage from "./pages/AnimatedPage";
 
+// Create a wrapper component that uses location to support AnimatePresence
+function AnimatedRoutes() {
+  const location = useLocation();
 
-function App() {
   return (
-    <div className="bg-gray-100">
-    <Router>
-      <Routes>
+    <AnimatePresence exitBeforeEnter>
+      <Routes location={location} key={location.pathname}>
         {/* Login page (no layout) */}
-        <Route path="/" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <AnimatedPage>
+              <Login />
+            </AnimatedPage>
+          }
+        />
 
         {/* Protected pages with Layout */}
         <Route element={<Layout />}>
-          <Route path="/home" element={<Dashboard/>} />
-          <Route path="/po1" element={<POPage/>} />
-          <Route path="/po2" element={<POPage/>} />
-          <Route path="/clearance1" element={<DispatchPage/>} />
-          <Route path="/clearance2" element={<DispatchPage/>} />
-                 <Route path="/store" element={<StorePage/>} />
-                     <Route path="/goods-issue-updates" element={<GoodsIssueUpdates/>} />
-                       <Route path="/projects" element={<ProjectsPage/>} />
-                        <Route path="/reports" element={<Reports/>} />
-                          <Route path="/users" element={<UsersPage/>} />
+          <Route
+            path="/home"
+            element={
+              <AnimatedPage>
+                <Dashboard />
+              </AnimatedPage>
+            }
+          />
+          <Route
+            path="/po1"
+            element={
+              <AnimatedPage>
+                <POPage />
+              </AnimatedPage>
+            }
+          />
+          <Route
+            path="/po2"
+            element={
+              <AnimatedPage>
+                <POPage />
+              </AnimatedPage>
+            }
+          />
+          <Route
+            path="/clearance1"
+            element={
+              <AnimatedPage>
+                <DispatchPage />
+              </AnimatedPage>
+            }
+          />
+          <Route
+            path="/clearance2"
+            element={
+              <AnimatedPage>
+                <DispatchPage />
+              </AnimatedPage>
+            }
+          />
+          <Route
+            path="/store"
+            element={
+              <AnimatedPage>
+                <StorePage />
+              </AnimatedPage>
+            }
+          />
+          <Route
+            path="/goods-issue-updates"
+            element={
+              <AnimatedPage>
+                <GoodsIssueUpdates />
+              </AnimatedPage>
+            }
+          />
+          <Route
+            path="/projects"
+            element={
+              <AnimatedPage>
+                <ProjectsPage />
+              </AnimatedPage>
+            }
+          />
+          <Route
+            path="/reports"
+            element={
+              <AnimatedPage>
+                <Reports />
+              </AnimatedPage>
+            }
+          />
+          <Route
+            path="/users"
+            element={
+              <AnimatedPage>
+                <UsersPage />
+              </AnimatedPage>
+            }
+          />
         </Route>
       </Routes>
-    </Router>
+    </AnimatePresence>
+  );
+}
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <Router>
+        <AnimatedRoutes />
+      </Router>
     </div>
   );
 }
